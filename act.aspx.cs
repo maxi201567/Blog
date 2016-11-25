ce563a2263a148fb168ec60ee3f668d5c79293c2
Maxi <maxi12896@live.com>
2016-11-25 12:15:14 +0800

    delete garbige

--- act.aspx.cs
+++
@@ -1,9 +0,0 @@
-public partial class AddNumber : System.Web.UI.Page
-{
-    private static int count = 1;
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        count ;
-        Response.Write("var pv=document.getElementById(pv); pv.innerText=" count ";");
-    }
-}
