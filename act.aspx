3365c45795c932ae5241cf3c82e672f2c459ae7b
Maxi <maxi12896@live.com>
2016-11-25 11:51:38 +0800

    add countting

---
+++ act.aspx
@@ -0,0 +1,1 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="act.aspx.cs" Inherits="AddNumber" %>
---
+++ act.aspx.cs
@@ -0,0 +1,9 @@
+public partial class AddNumber : System.Web.UI.Page
+{
+    private static int count = 1;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        count ;
+        Response.Write("var pv=document.getElementById(pv); pv.innerText=" count ";");
+    }
+}
