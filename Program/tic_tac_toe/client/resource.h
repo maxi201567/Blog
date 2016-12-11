7d4f2b598b4f5a235cb077d2780bb51da1ed6d21
Maxi <maxi12896@live.com>
2016-12-11 22:59:10 +0800

    transparent

---
+++ Program/tic_tac_toe/client/resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 井.rc 使用
+//
+#define IDR_MENU                        101
+#define IDD_DIALOG                      102
+#define IDI_ICON                        103
+#define IDB_ICON                        104
+#define IDB_BACK                        105
+#define IDB_PNG1                        105
+#define IDB_PNG2                        106
+#define IDC_EDIT                        1021
+#define IDR_IP                          10101
+#define IDR_READY                       10102
+#define IDR_EXIT                        10103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
