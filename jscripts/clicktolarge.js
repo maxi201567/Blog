2b02e9242110fee4f2e940225a0a2f8d3df46dc1
Maxi <maxi12896@live.com>
2017-02-19 13:53:14 +0800

    add idea

---
+++ jscripts/clicktolarge.js
@@ -0,0 +1,15 @@
+function elementDisplay(obja, objb)
+{
+	var a=document.getElementById(obja);
+	var b=document.getElementById(objb);
+	if(b.style.display != 'none')
+	{
+		a.innerHTML='+';
+		b.style.display='none';
+	}
+    else
+	{	
+        a.innerHTML='-';
+        b.style.display='block';
+	}
+}
